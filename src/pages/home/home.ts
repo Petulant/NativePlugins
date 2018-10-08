9aa938c392fe0da75edb824be9bbf7b615d25941
Thembelihle <pmokhwebane@gmail.com>
2018-10-08 13:13:46 +0200

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { MediaCapture, MediaFile, CaptureError, CaptureImageOptions, CaptureVideoOptions, CaptureAudioOptions } from '@ionic-native/media-capture';
+
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,31 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,private mediaCapture: MediaCapture) {
 
   }
-
+  Camera(){
+    let options: CaptureImageOptions = { limit: 1 };
+    this.mediaCapture.captureImage(options)
+  .then(
+    (data: MediaFile[]) => console.log(data),
+    (err: CaptureError) => console.error(err)
+  ); 
+  }
+  Video(){
+    let options: CaptureVideoOptions = { limit: 1 };
+    this.mediaCapture.captureVideo(options)
+  .then(
+    (data: MediaFile[]) => console.log(data),
+    (err: CaptureError) => console.error(err)
+  ); 
+  }
+  Audio(){
+    let options: CaptureAudioOptions = { limit: 1 };
+    this.mediaCapture.captureAudio(options)
+  .then(
+    (data: MediaFile[]) => console.log(data),
+    (err: CaptureError) => console.error(err)
+  ); 
+  }
 }
